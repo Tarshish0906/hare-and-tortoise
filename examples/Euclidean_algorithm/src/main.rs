312b4fa5650fe80331404e6d5ce8e876ee186c64
Tarshish0906 <k.oxygen08@gmail.com>
2021-03-11 19:49:58 +0900

    Ver0.1.1

--- examples/Euclidean_algorithm/src/main.rs
+++ examples/Euclidean_algorithm/src/main.rs
@@ -1,4 +1,4 @@
-//ユークリッドの互除法
+// ユークリッドの互除法
 
 use nannou::prelude::*;
 
@@ -16,16 +16,22 @@ fn model(_app: &App) -> Model {
 }
 
 fn event(_app: &App, _model: &mut Model, _event: Event) {
-    //横縦比が num_a : num_b の長方形によって分割
+    // 横縦比が num_a : num_b の長方形によって分割
     let num_a = 10;
     let num_b = 6;
     
-    //長方形の拡大倍率
+    // 長方形の拡大倍率
     let scalar = 50;
 
-    //数値の大きさを拡大
+    // 数値の大きさを拡大
     let num_a *= scalar;
     let num_b *= scalar;
+
+    // プログラム実行中に動く変数
+    // 分割に使う正方形の幅の大きさ（初期値 num_b）
+    let wd = num_b;
+
+
 
 }
 
