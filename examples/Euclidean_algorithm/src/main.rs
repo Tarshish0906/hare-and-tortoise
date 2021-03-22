5d634853273aaa819857e2f16d58cdd97aefe24f
Tarshish0906 <k.oxygen08@gmail.com>
2021-03-23 03:22:27 +0900

    Ver0.1.2

--- examples/Euclidean_algorithm/src/main.rs
+++ examples/Euclidean_algorithm/src/main.rs
@@ -1,6 +1,26 @@
 // ユークリッドの互除法
 use nannou::prelude::*;
 
+// アプリケーションの状態を定義
+struct Model {}
+
+// アプリケーションの開始
 fn main() {
-    println!("Hello, world!");
+    nannou::app(model)
+        .event(event)
+        .simple_window(view)
+        .run();
+}
+
+// Model のインスタンスを生成
+fn model(_app: &App) -> Model {
+    Model {}
+}
+
+// イベントハンドラ
+fn event(_app: &App, _model: &mut Model, _event: Event) {
+}
+
+// 画面の描画処理
+fn view(_app: &App, _model: &Model, _frame: Frame) {
 }
